833bb1f8853d2388981f3223596fb305672bfe2a
KERDJOUDJ Mohamed <mohamed.kerdjoudj@hotmail.com>
2022-08-11 16:18:33 +0100

    latest version

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="splash-container">
+        <div class="splash">
+          <h1>Splendid Food</h1>
+        </div>
+      </div>
+
+      <main class="wrapper">
+
+        <h2>Recommended</h2>
+
+        <div class="recommended">
+          <product-cart
+            v-for="(product,index)  in inventory.slice(0,3)"
+              :key="product"
+              :index="index"
+              :product="product"
+              :addToCart="addToCart"
+              class="card">
+
+          </product-cart>
+
+        </div>
+
+      </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import productCart from '@/components/ProductCart-component.vue'
 
 export default {
   name: 'HomeView',
+  props: ['inventory', 'addToCart'],
   components: {
-    HelloWorld
+    productCart
   }
 }
 </script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+      <product-cart
+        v-for="(product,index)  in inventory.slice(0,6)"
+        :key="product"
+        :index="index"
+        :product="product"
+        :addToCart="addToCart"
+        class="card">
+    </product-cart>
+    </div>
+  </main>
+</template>
+<script>
+import productCart from '@/components/ProductCart-component.vue'
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    productCart
+  }
+}
+</script>
